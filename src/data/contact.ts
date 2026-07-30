0dae0fdcdb1c9aa340eb484bd70671d7c5277515
Mohsin Javeed <271045510@formanite.fccollege.edu.pk>
2026-07-30 20:46:33 +0500

    fix: Terms n Conditions bugs fixed

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,5 +1,5 @@
 export const CONTACT_INFO = {
-  phoneDisplay: "03300599814",
-  phoneDial: "tel:+443300599814",
-  email: "info@innovstoretech.store"
+  phoneDisplay: "0330 059 7119",
+  phoneDial: "tel:+443300597119",
+  email: "info@novaconcerns.site"
 };
